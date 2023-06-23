6f8c1011b3cf01cd43b2944db3cda8ec21792c6b
Mohammad Aseem <aseemmohammad73@gmail.com>
2023-06-23 23:57:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+ 
+
+  return (
+    <main>
+      <Navbar/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+
+const initialState={
+  cartItems:[],
+  amount:5,
+  total:0,
+  isLoading:true,
+}
+
+
+const cartSlice=createSlice({
+  name:'cart',
+  initialState
+})
+console.log(cartSlice);
+
+export default cartSlice
